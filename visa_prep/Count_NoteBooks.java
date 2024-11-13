e3c9c8d09bf2cb6f429d1900539f298f53916aa4
sri7889 <141262614+sri7889@users.noreply.github.com>
2024-11-13 23:54:57 +0530

    Create Count_NoteBooks.java

---
+++ visa_prep/Count_NoteBooks.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scan = new Scanner(System.in);
+        int pulp = scan.nextInt();
+        
+        System.out.println((pulp*1000)/100);
+    }
+}
